55df8e3c7fc583107d5dc86368caea66980b2333
Apoorv Pranjal <appu2221@gmail.com>
2022-11-02 20:34:16 +0530

    added drawerlayout with map button to display the map view

--- app/src/main/java/com/example/happypets/Activity/MainActivity.java
+++ app/src/main/java/com/example/happypets/Activity/MainActivity.java
@@ -1,15 +1,49 @@
 package com.example.happypets.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 import com.example.happypets.Adapters.TabLayoutAdapter;
+import com.example.happypets.Map.MapsActivity;
 import com.example.happypets.R;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    // creating a Tag string
+    private final String TAG = MainActivity.class.getSimpleName();
+
+    //creating variables for adding functionality to the drawer layout
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        if(item.getItemId() == R.id.map_button){
+            Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+            startActivity(i);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +58,59 @@ public class MainActivity extends AppCompatActivity {
         //setting viewpager to tab layout
         TabLayout mainTab = findViewById(R.id.mainTab);
         mainTab.setupWithViewPager(viewPager);
+
+        /* declaring objects needed for navigation view */
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.drawer_open,R.string.drawer_close);
+        /* setting up drawer layout */
+        navigationView.bringToFront(); // making drawer to come to the front of the screen
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // make burger button to become back button
+        // setting listener when options are selected on the navigation view
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.drawer_layout_profile:
+                        Log.e(TAG,"profile");
+                        break;
+                    case R.id.drawer_layout_chat:
+                        Log.e(TAG,"chat");
+                        break;
+                    case R.id.drawer_layout_fav:
+                        Log.e(TAG,"fav");
+                        break;
+                    case R.id.drawer_layout_share:
+                        Log.e(TAG,"share");
+                        break;
+                    case R.id.drawer_layout_logout:
+                        Log.e(TAG,"logout");
+                        break;
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.map_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else
+            super.onBackPressed();
+    }
+
 }
 
 
