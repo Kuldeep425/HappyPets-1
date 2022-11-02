b7c525be2e917c1278942f521fa05e359d9f1dc6
Apoorv Pranjal <appu2221@gmail.com>
2022-11-02 13:53:03 +0530

    added tab layout and viewpager to switch between fragments

--- app/src/main/java/com/example/happypets/Activity/MainActivity.java
+++ app/src/main/java/com/example/happypets/Activity/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.happypets.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.happypets.Adapters.TabLayoutAdapter;
 import com.example.happypets.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setting viewpager
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        //setting adapter to viewpager
+        TabLayoutAdapter tabLayoutAdapter = new TabLayoutAdapter(getSupportFragmentManager(), this);
+        viewPager.setAdapter(tabLayoutAdapter);
+        //setting viewpager to tab layout
+        TabLayout mainTab = findViewById(R.id.mainTab);
+        mainTab.setupWithViewPager(viewPager);
     }
 }
 
