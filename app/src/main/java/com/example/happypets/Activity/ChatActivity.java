73eed8097986a7552d19897153199a603d42d9cb
Apoorv Pranjal <appu2221@gmail.com>
2022-11-09 18:16:42 +0530

    removed all merge conflicts

---
+++ app/src/main/java/com/example/happypets/Activity/ChatActivity.java
@@ -0,0 +1,32 @@
+package com.example.happypets.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
+
+import com.example.happypets.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ChatDetailActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+}
